da25f2d6eed28f03e31babac33e843a0b95081ad
OisinWatchornLyness <20112598@mail.wit.ie>
2026-09-18 14:45:42 +0100

    Added keys to tasks

--- firstReact/src/App.jsx
+++ firstReact/src/App.jsx
@@ -6,13 +6,14 @@ import React, { useState } from 'react';
 function App() {
 
 
-    const [ taskState, setTaskState ] = useState({
+  const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
+
 
 
 
@@ -24,6 +25,7 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          key={task.id}
         />
       ))} 
     </div>
